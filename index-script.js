321e26fce52f7b174dae9e7812acd45897548bf4
Yousaf Anwar <yousafanwar74@gmail.com>
2023-11-01 11:23:37 +0400

    commit

--- index-script.js
+++ index-script.js
@@ -16,16 +16,10 @@ function intersectionObserveFunc(entries, options){
                 
                 if(entry.classList.contains("index-sec2-text-slideDown")){
                     entry.classList.remove("index-sec2-text-slideDown");
-                    entry.classList.add("index-sec2-text-slideUp");
-                    
+                    entry.classList.add("index-sec2-text-slideUp");                    
                 }
-
-            })
-            
-        }
-    })
-}
-
+            })}
+    })}
 let options = {
     root: null,
     threshold: 0.5,
@@ -36,3 +30,24 @@ let observer = new IntersectionObserver(intersectionObserveFunc, options);
 if(target){
     observer.observe(target);
 }
+
+////////////////////////////////////////////////////////////////////////
+
+let sec4Target = document.querySelector(".sec-4-text");
+let observer1 = new IntersectionObserver(intersectionObserveFunc2, options);
+
+
+function intersectionObserveFunc2(entries, options){
+    entries.forEach(function(entry){
+        if(entry.isIntersecting){
+            
+            sec4Target.classList.remove("sec-4-text");
+            sec4Target.classList.add(".sec-4-text-fadeIn");   
+        }
+    })
+}
+
+if(sec4Target){
+    observer1.observe(sec4Target);
+}
+
